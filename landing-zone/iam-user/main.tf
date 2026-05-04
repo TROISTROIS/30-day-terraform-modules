80086fc32d6f8545afd24923c5362bf0baa561ec
TROISTROIS <franciscangeno1@gmail.com>
2026-05-04 13:13:23 +0300

    Added IAM policies for cloudwatch

--- landing-zone/iam-user/main.tf
+++ landing-zone/iam-user/main.tf
@@ -2,6 +2,36 @@ resource "aws_iam_user" "IAMuser" {
     name = var.user_name
 }
 
+resource "aws_iam_policy" "cloudwatch_read_only" {
+    name = "cloudwatch-read-only"
+    policy = data.aws_iam_policy_document.cloudwatch_read_only.json
+}
+
+data "aws_iam_policy_document" "cloudwatch_read_only" {
+    statement {
+        effect = "Allow"
+        actions = [
+            "cloudwatch:Describe*",
+            "cloudwatch:Get*",
+            "cloudwatch:List*"
+            ]
+            resources = ["*"]
+            }
+            }
+
+resource "aws_iam_policy" "cloudwatch_full_access" {
+    name = "cloudwatch-full-access"
+    policy = data.aws_iam_policy_document.cloudwatch_full_access.json
+    }
+
+data "aws_iam_policy_document" "cloudwatch_full_access" {
+    statement {
+        effect = "Allow"
+        actions = ["cloudwatch:*"]
+        resources = ["*"]
+        }
+        } 
+
 resource "aws_iam_user_policy_attachment" "dev_cloudwatch_full_access" {
     count = var.give_dev_cloudwatch_full_access ? 1 : 0
 
