4f6adff054000cad709eedea7ca1512b7064596b
TROISTROIS <franciscangeno1@gmail.com>
2026-05-05 14:25:41 +0300

    Removed the Policies from the module

--- landing-zone/iam-user/main.tf
+++ landing-zone/iam-user/main.tf
@@ -2,46 +2,16 @@ resource "aws_iam_user" "IAMuser" {
     name = var.user_name
 }
 
-resource "aws_iam_policy" "cloudwatch_read_only" {
-    name = "cloudwatch-read-only"
-    policy = data.aws_iam_policy_document.cloudwatch_read_only_policy.json
-}
-
-data "aws_iam_policy_document" "cloudwatch_read_only_policy" {
-    statement {
-        effect = "Allow"
-        actions = [
-            "cloudwatch:Describe*",
-            "cloudwatch:Get*",
-            "cloudwatch:List*"
-            ]
-            resources = ["*"]
-            }
-            }
-
-resource "aws_iam_policy" "cloudwatch_full_access" {
-    name = "cloudwatch-full-access"
-    policy = data.aws_iam_policy_document.cloudwatch_full_access_policy.json
-    }
-
-data "aws_iam_policy_document" "cloudwatch_full_access_policy" {
-    statement {
-        effect = "Allow"
-        actions = ["cloudwatch:*"]
-        resources = ["*"]
-        }
-        } 
-
 resource "aws_iam_user_policy_attachment" "dev_cloudwatch_full_access" {
     count = var.give_dev_cloudwatch_full_access ? 1 : 0
 
     user = aws_iam_user.IAMuser.name
-    policy_arn = aws_iam_policy.cloudwatch_full_access.arn 
+    policy_arn = var.cloudwatch_full_access_arn 
 }
 
 resource "aws_iam_user_policy_attachment" "dev_cloudwatch_read_only" {
     count = var.give_dev_cloudwatch_full_access ? 0 : 1
     
     user = aws_iam_user.IAMuser.name
-    policy_arn = aws_iam_policy.cloudwatch_read_only.arn 
+    policy_arn = var.cloudwatch_read_only_arn 
 }
--- landing-zone/iam-user/variables.tf
+++ landing-zone/iam-user/variables.tf
@@ -7,3 +7,13 @@ variable "give_dev_cloudwatch_full_access" {
     description = "If true, dev gets CloudWatch Full Access"
     type = bool
 }
+
+variable "cloudwatch_read_only_arn" {
+    description = "The ARN of the CloudWatch Read Only policy"
+    type        = string
+}
+
+variable "cloudwatch_full_access_arn" {
+    description = "The ARN of the CloudWatch Full Access policy"
+    type        = string
+}
