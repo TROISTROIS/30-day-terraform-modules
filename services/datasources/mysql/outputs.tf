af54897ace9dfca91094d5299c87125f1381991f
TROISTROIS <franciscangeno1@gmail.com>
2026-05-15 14:33:41 +0300

    Added more output variables

--- services/datasources/mysql/outputs.tf
+++ services/datasources/mysql/outputs.tf
@@ -1,9 +1,19 @@
-output "rds_endpoint" {
+output "RDS_endpoint" {
     description = "The connection endpoint for the RDS instance"
     value = aws_db_instance.RDS_instance.endpoint
 }
 
-output "arn" {
+output "Address" {
+    description = "The ARN of the RDS instance"
+    value = aws_db_instance.RDS_instance.address 
+}
+
+output "ARN" {
     description = "The ARN of the RDS instance"
     value = aws_db_instance.RDS_instance.arn 
+}
+
+output "Port" {
+    description = "The port the database is listening on"
+    value = aws_db_instance.RDS_instance.port
 }
