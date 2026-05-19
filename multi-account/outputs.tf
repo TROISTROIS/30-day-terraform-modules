fa6f2d62e132dcd1e9b0f16e0f4c10b882edaec8
TROISTROIS <franciscangeno1@gmail.com>
2026-05-19 13:01:17 +0300

    Exposed outputs

---
+++ multi-account/outputs.tf
@@ -0,0 +1,8 @@
+output "stage_account" {
+value = data.aws_caller_identity.stage.account_id
+description = "The ID of the stage AWS account"
+}
+output "prod_account" {
+value = data.aws_caller_identity.prod.account_id
+description = "The ID of the prod AWS account"
+}
