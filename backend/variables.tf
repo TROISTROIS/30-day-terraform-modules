9a7c4a044373252f09c9e05cb029edbb0fcca95d
TROISTROIS <franciscangeno1@gmail.com>
2026-05-29 15:39:09 +0300

    Added Test environment variables

--- backend/variables.tf
+++ backend/variables.tf
@@ -7,3 +7,18 @@ variable "dynamodb-backend-table" {
     description = "The name of the dynamodb table"
     type = string
 }
+
+variable "environment" {
+    description = "Environment I am working on"
+    type = string
+
+    validation {
+        condition = contains(["Stage", "Production", "Test"], var.environment)
+        error_message = "Environment must be Stage , Test or Production. "
+    }
+}
+
+variable "day" {
+    description = "The day of the challenge"
+    type = number
+}
