1ef580ef801d569d6e03e8e211232504cd08c5a8
TROISTROIS <franciscangeno1@gmail.com>
2026-05-29 15:41:02 +0300

    Added user data script for the server to use during bootstrapping

---
+++ cluster/services/hello-world-app/user-data.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Fetch the instance ID and local IP using the EC2 metadata service
+# Note: Using token-based IMDSv2 (Standard for modern AMIs)
+TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
+INSTANCE_ID=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/instance-id)
+LOCAL_IP=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/local-ipv4)
+
+cat > index.html <<EOF
+<h1>${server_text}</h1>
+<p>This is Day ${day}!</p>
+<p>Instance ID: $INSTANCE_ID</p>
+<p>Local IP: $LOCAL_IP</p>
+EOF
+
+nohup busybox httpd -f -p ${server_port} &
+
